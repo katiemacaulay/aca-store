0d4587d390fdd70331a8e373b6404751939536ef
katiemacaulay <katiemmacaulay@gmail.com>
2019-01-09 14:26:00 -0600

    cleaned up using a mainPage function. created a review button that functions, working on making a shopping cart

--- public/index.js
+++ public/index.js
@@ -1,22 +1,28 @@
 "use strict";
-// display all front page products
-function mainPage(){
-    document.getElementById("itemlist").innerHTML = products.map((item,index)=>{
+
+function mainPage(productsData){
+    document.getElementById("itemlist").innerHTML = productsData.map((item,index)=>{
     return `<img src=${item.imgUrl} alt=${item.description}/>
-    <li>${item.name}, Stars: ${item.rating}, Reviews: ${item.reviews.length} 
-    From: ${item.price} <button onClick="showProduct(${index})" id="">See Details</button></li>`
+    <li>${item.name}, 
+    Stars: ${item.rating}, 
+    Reviews: ${item.reviews.length} 
+    From: ${item.price} 
+    <button onClick="showProduct(${index})" id="">See Details</button> </li>`
   }).join('');
 }
-mainPage()
+mainPage(products)
 
-  
 function showProduct(productNumber){
     let product = products[productNumber]
     document.getElementById("itemlist").innerHTML =
         `<img src=${product.imgUrl} alt=${product.description}/>
-        <li>${product.name}, Stars: ${product.rating}, Reviews: ${product.reviews.length} 
-        From: ${product.price}, Description: ${product.description}, ${product.category}
-        , ${product.description}<button onClick="showReviews(${productNumber})"> Show Reviews </button>
+        <li>${product.name}, 
+        Stars: ${product.rating}, 
+        Reviews: ${product.reviews.length} 
+        From: ${product.price}, 
+        Description: ${product.description}, 
+        ${product.category}
+        <button onClick="showReviews(${productNumber})"> Show Reviews </button>
         <button onClick="addToCart()" id="">Add to Cart</button></li>`
 }
 
@@ -24,8 +30,8 @@ function showReviews(productNumber){
     let product = products[productNumber].reviews
     document.getElementById("reviews").innerHTML =
      product.map((review, index) => {
-         return review.rating
-     })
+         return `<li>User Rating: ${review.rating} </br> ${review.description} </li>`
+     }).join('');
 }
 
 function searching(){
@@ -38,9 +44,6 @@ function searching(){
             }
         })
     })
-return document.getElementById('itemlist').innerHTML = foundProducts.map(product => 
-    `<img src=${product.imgUrl} alt=${product.description}/>
-    <li>${product.name}, Stars: ${product.rating}, Reviews: ${product.reviews.length} 
-    From: ${product.price} <button onClick="showProduct()">See Details</button></li>`).join('')
+return mainPage(foundProducts)
 }
 
