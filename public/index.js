6355de196f6184b91eb224e2d6dd479b105431d0
katiemacaulay <katiemmacaulay@gmail.com>
2019-01-09 10:45:24 -0600

    added a product showcase page

--- public/index.js
+++ public/index.js
@@ -3,9 +3,16 @@
 document.getElementById("itemlist").innerHTML = products.map((item,index)=>{
     return `<img src=${item.imgUrl} alt=${item.description}/>
     <li>${item.name}, Stars: ${item.rating}, Reviews: ${item.reviews.length} 
-    From: ${item.price} <button>See Details</button></li>`
+    From: ${item.price} <button onClick="showProduct(${index})" id="">See Details</button></li>`
   }).join('');
   
+function showProduct(productNumber){
+    let product = products[productNumber]
+    document.getElementById("itemlist").innerHTML =
+        `<img src=${product.imgUrl} alt=${product.description}/>
+        <li>${product.name}, Stars: ${product.rating}, Reviews: ${product.reviews.length} 
+        From: ${product.price} <button onClick="addToCart()" id="">Add to Cart</button></li>`
+}
 
 function searching(){
     let foundProducts = [];
@@ -20,6 +27,6 @@ function searching(){
 return document.getElementById('itemlist').innerHTML = foundProducts.map(product => 
     `<img src=${product.imgUrl} alt=${product.description}/>
     <li>${product.name}, Stars: ${product.rating}, Reviews: ${product.reviews.length} 
-    From: ${product.price} <button>See Details</button></li>`).join('')
+    From: ${product.price} <button onClick="showProduct()">See Details</button></li>`).join('')
 }
 
