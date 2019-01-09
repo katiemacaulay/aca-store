f99cbf235c9e70f44fd68c6169a5c7100ef961b5
katiemacaulay <katiemmacaulay@gmail.com>
2019-01-09 13:06:30 -0600

    made a home button, show reviews button, and shopping cart.

--- public/index.js
+++ public/index.js
@@ -1,17 +1,31 @@
 "use strict";
 // display all front page products
-document.getElementById("itemlist").innerHTML = products.map((item,index)=>{
+function mainPage(){
+    document.getElementById("itemlist").innerHTML = products.map((item,index)=>{
     return `<img src=${item.imgUrl} alt=${item.description}/>
     <li>${item.name}, Stars: ${item.rating}, Reviews: ${item.reviews.length} 
     From: ${item.price} <button onClick="showProduct(${index})" id="">See Details</button></li>`
   }).join('');
+}
+mainPage()
+
   
 function showProduct(productNumber){
     let product = products[productNumber]
     document.getElementById("itemlist").innerHTML =
         `<img src=${product.imgUrl} alt=${product.description}/>
         <li>${product.name}, Stars: ${product.rating}, Reviews: ${product.reviews.length} 
-        From: ${product.price} <button onClick="addToCart()" id="">Add to Cart</button></li>`
+        From: ${product.price}, Description: ${product.description}, ${product.category}
+        , ${product.description}<button onClick="showReviews(${productNumber})"> Show Reviews </button>
+        <button onClick="addToCart()" id="">Add to Cart</button></li>`
+}
+
+function showReviews(productNumber){
+    let product = products[productNumber].reviews
+    document.getElementById("reviews").innerHTML =
+     product.map((review, index) => {
+         return review.rating
+     })
 }
 
 function searching(){
