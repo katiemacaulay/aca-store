72c7431d8dacb57a8fbdb22514f84fb45a7680ba
katiemacaulay <katiemmacaulay@gmail.com>
2019-01-15 16:24:07 -0600

    created category search, and simple fixes

--- public/index.js
+++ public/index.js
@@ -12,6 +12,7 @@ function mainPage(productsData){
         Stars: ${item.rating}
         Reviews: ${item.reviews.length} 
         From: ${item.price} 
+        Category: ${item.category}
     <button onClick="showProduct(${index})">See Details</button> 
     <button onClick="addToCart(${index})">Add to Cart</button></li>`
   });
@@ -24,6 +25,7 @@ window.onload = function(){
         cart=[];
     }  
     pageAlert(); 
+    findCategories();
 }
 
 function showProduct(productNumber){
@@ -65,18 +67,47 @@ function searching(){
 return mainPage(foundProducts)
 }
 
+function findCategories(){
+    var categories = document.getElementById("categories")
+    let productCategories = []
+    categories.text = products.map(item => {
+        if(productCategories.includes(item.category)){
+            return
+        } else {
+            productCategories.push(item.category)
+        }
+    })
+    return document.getElementById("categories").innerHTML = 
+    productCategories.map(item => {
+        return `<option id="${item}">${item}</option>`
+    })
+}
+
+document.getElementById("categories").onchange = options
+
+function options(e){
+    let chosenCategory = e.target.value
+    let categoryReturnList = []
+    products.map(item => {
+        if(chosenCategory == item.category){
+            categoryReturnList.push(item)
+        }
+    })
+    mainPage(categoryReturnList)
+}
+
 function viewCart(){
     document.getElementById("reviews").innerHTML = ""
     document.getElementById("itemlist").innerHTML =
     cart.map((products, index) => {
-        return `<li>${products}</li>
+        return `<li>${products[0]}</li>
         <button onclick="removeProduct(${index})"> remove </button>`
-    }).join('')
+    }).join('') + '<br/><button onclick="checkoutForm()">checkout</button>'
 }
 
 function addToCart(productNumber){
     let product = products[productNumber]
-    cart.push(product.name)
+    cart.push([product.name, product.price])
     sessionStorage.setItem("product", JSON.stringify(cart))
 }
 
@@ -84,6 +115,43 @@ function removeProduct(index){
     cart.splice(index, 1)
     viewCart();
     sessionStorage.setItem("product", JSON.stringify(cart))
+}
+
+function cartSum(){
+    return cart.reduce((total, itemPrice) => {
+        let price = itemPrice[1].substr(1)
+        return total + parseFloat(price)
+    }, 0)
+}
+
+function checkoutForm(){
+    let cartAmount = cartSum()
+    document.getElementById("itemlist").innerHTML = 
+    `<div>Current Subtotal: $${cartAmount}</div>
+    <div>${cart.length} items in cart </div>
+    <form>
+  <div>
+      <h1>Checkout
+        <span>$${cartAmount}</span>
+      </h1>
+    </div>
+    <p>
+      <input type="text" placeholder="Your name" autofocus/>
+      <input type="text" placeholder="MM"/>
+      <input type="text" placeholder="YY"/>
+    </p>
+    <p>
+      <input type="text" placeholder="4111 1111 1111 1111"/>
+      <input type="text" placeholder="CVC"/>
+    </p>
+    <p>
+      <input type="submit" value="Purchase onclick="purchaseCompleting()"/>
+    </p>
+  </form>`
+}
+
+function purchaseCompleting(){
+    alert('Purchase is completed!')
 }
 
 let settingAlert = () => {
